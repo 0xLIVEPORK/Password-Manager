94ce866e77657b36e75d3b6f44861bbd538f3e73
EVILPORK <0xlivepork@gmail.com>
2025-03-31 22:38:04 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -7,6 +7,8 @@
 
 std::string generatePassword(int length);
 std::string xorecrypt(std::string pass);
+std::string key = "notapassword"; 
+std::string xordecrypt(std::string encrypted_pass);
 
 int main() {
     srand(time(0));
@@ -29,7 +31,13 @@ int main() {
     std::string newpass = generatePassword(length);
     std::cout << "Your Password: " << newpass << "\n";
 
-    std::cout << xorecrypt(newpass);
+    std::string encrypted = xorecrypt(newpass);
+    std::cout << encrypted << std::endl;
+
+
+    std::string decrypted = xordecrypt(encrypted);
+    std::cout << "\n" << decrypted ;
+
 return EXIT_SUCCESS;
 }
 
@@ -51,12 +59,21 @@ std::string generatePassword(int length) {
 
 //I get that this should not be visible but this is xor encryption as a temporary bandaid
 std::string xorecrypt(std::string pass) {
-    std::string key = "notapassword"; 
     std::string encryptedpass = pass;
     for(int i = 0; i < pass.length(); i++)
     {
         encryptedpass.at(i) ^= key[i % key.length()];
     }
-    return encryptedpass; //To Do: Decrypt function & Menu
+    return encryptedpass;
 }
 
+std::string xordecrypt(std::string encrypted_pass) {
+    std::string decrypted = encrypted_pass;
+
+    for(int i = 0; i < encrypted_pass.length(); i++)
+    {
+        decrypted.at(i) ^= key[i % key.length()];
+    }
+
+    return decrypted;
+}
