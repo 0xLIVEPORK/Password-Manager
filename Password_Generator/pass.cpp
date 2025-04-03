60fd7872d9ab7f65fb0a5fd99e63438026174ab2
EVILPORK <0xlivepork@gmail.com>
2025-04-03 23:03:56 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -88,3 +88,37 @@ void save_file() {
     //Save what we have done
     //Only 5 save file
 }
+
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <limits>
+#include <fstream>
+
+struct keypass{
+    std::string password;
+    std::string encrypted;
+    std::string decrypter = "Nee Kami Sama";
+
+    void Menu_Loop(std::string &password)
+    {
+    std::cout << 
+    "[1] - Generate A Password\n"
+    "[2] - Save a Log-in\n"
+    "[3] - View Saved Log-ins\n\n"
+    "Enter a Valid Integer: ";
+
+    std::cin >> password;
+    }
+};
+
+
+int main() {
+    
+
+    keypass.Menu_Loop();
+    
+return EXIT_SUCCESS;
+}
