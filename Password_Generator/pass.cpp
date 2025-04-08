a6e4bdaf3be127deefebcb57892a247e9b5e4573
EVILPORK <0xlivepork@gmail.com>
2025-04-08 23:37:40 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -1,114 +1,79 @@
 #include <iostream>
 #include <cstdlib>
+#include <limits>
 #include <string>
 #include <ctime>
-#include <limits>
-#include <fstream>
+// #include <fstream> - Out of Scope 
 
-struct keypass{
-  std::string password;
-  std::string encrypted;
-  std::string decrypter;
-  };
-
-
-// Functions
-void Menu_Loop();
-
-std::string Password(std::string &Password, std::string &Encrypted);
+std::string generatePassword(int length);
+std::string xorecrypt(std::string pass);
+std::string key = "notapassword"; 
+std::string xordecrypt(std::string encrypted_pass);
 
 int main() {
-  srand(time(0));
+    srand(time(0));
 
-    Menu_Loop();
-    
+    int length;
+
+    std::cout << "Enter The Length of Password Generated: ";
+    while(true) 
+    {
+        if(!(std::cin >> length) || length < 0 || length > 200)
+        {
+            std::cerr << "Invalid Input: ";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+            continue;
+        }
+        break;
+    }
+
+    std::string newpass = generatePassword(length);
+    std::cout << "Your Password: " << newpass << "\n";
+
+    std::string encrypted = xorecrypt(newpass);
+    std::cout << encrypted << std::endl;
+
+
+    std::string decrypted = xordecrypt(encrypted);
+    std::cout << "\n" << decrypted ;
+
 return EXIT_SUCCESS;
 }
 
-void Menu_Loop()
-{
-    int choice;
-    int tries = 0;
-    std::string rand_encrypter[5] = 
-    {
-      "NEE KAMISAMA", 
-      "Oni Gae Da Yo", 
-      "Idk what the fuck the password is", 
-      "A number 7 large", 
-      "What the fuck am I typing"
-    };
+// Functions
+std::string generatePassword(int length) {
+    const std::string chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789"
+        "!@#$%^&*()";
 
-    keypass kp;
-    kp.encrypted = rand_encrypter[rand() % (sizeof(rand_encrypter) / sizeof(rand_encrypter[0]))];
-
-    std::cout << 
-    "[1] - Generate A Password\n"
-    "[2] - Save a Log-in\n"
-    "[3] - View Saved Log-ins\n\n"
-    "Enter a Valid Integer: ";
-
-    do
-    {
-        if(tries > 4)
-          {
-            std::cout << "You have tried to many times";
-            break;
-          } else if(!(std::cin >> choice) || choice <= 0 || choice > 3) 
-          {
-          tries++;
-          std::cerr << "Invalid Input\nTry Again: ";
-          std::cin.clear();
-          std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-          std::cout << tries;
-          continue;
-          }
-    break;
+    std::string password;
+    for (int i = 0; i < length; ++i) {
+        password += chars[rand() % chars.size()];
     }
-    while(true);
-
-    switch(choice)
-    {
-      case 1:
-      std::cout << Password(kp.password, kp.encrypted);
-      break;
-    }
-
-    
-
+    return password;
 }
 
-std::string Password(std::string &Password, std::string &Encrypted)
-{
-  int length;
-  std::string chars = 
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  "abcdefghijklmnopqrstuvwxyz"
-  "0123456789"
-  "!@#$%^&*()";
 
-  while(true)
-  {
-    if(!(std::cin >> length) || length <= 0 || length > 100)
+//I get that this should not be visible but this is xor encryption as a temporary bandaid
+std::string xorecrypt(std::string pass) {
+    std::string encryptedpass = pass;
+    for(int i = 0; i < pass.length(); i++)
     {
-      std::cerr << "\nLength is Invalid\nTry Again: ";
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      continue;
+        encryptedpass.at(i) ^= key[i % key.length()];
     }
-  break;
-  }
-  for(int i = 0; i < length; i++)
-  {
-    Password += chars[rand() % chars.length()];
-  }
+    return encryptedpass;
+}
 
-  //encryption
-  std::string Test = Password;
-  for(int i = 0; i < length; i++)
-  {
-    Test.at(i) ^= Encrypted[i % Encrypted.length()];
-  }
-  
-  std::cout << "\n" << Test;
-  return Password;
+std::string xordecrypt(std::string encrypted_pass) {
+    std::string decrypted = encrypted_pass;
+
+    for(int i = 0; i < encrypted_pass.length(); i++)
+    {
+        decrypted.at(i) ^= key[i % key.length()];
+    }
+
+    return decrypted;
 }
