f407487469576f90c65eead88646355514ad2e98
EVILPORK <0xlivepork@gmail.com>
2025-04-10 22:55:35 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -14,6 +14,24 @@ std::string Generate_Password(unsigned short length)
   std::string Generated;
   for(int i = 0; i < length; i++)
   {
-    Generated += chars[rand() % chars.length()]; //Generated concatenate everytime the for loop runs until it reaches (length - 1) rand gives a huge number between 0 and 32767 remainder %, you might question what if rand = 0 woudn't that be division by 0? But that's not true because the modulo operator is not actually a division it just gives you the remainder and the programmers of t hat funciton are smart enough to think aobut the edge cases like what if hte input is 0.
+    Generated += chars[rand() % chars.length()];
   }
+
+  return Generated;
+}
+
+int main() {
+  unsigned short length;
+
+  std::cout << "Enter a Number";
+
+  while(true)
+  {
+    if(!(std::cin >> length) || length < 0 || length > 250)
+    {
+      std::cerr << "You have Inputted an \"INVALID SHORT INTEGER\"\nTry Again: "
+    }
+  }
+
+  return EXIT_SUCCESS;
 }
