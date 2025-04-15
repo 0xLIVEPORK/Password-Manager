2520dad7624b9aad2c76c4cb54d88a5f16e2f365
EVILPORK <0xlivepork@gmail.com>
2025-04-15 21:55:22 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -15,75 +15,100 @@ struct PW_Management
     "IDK WHAT TF IM DOING"
   };
 
-  std::string random_word;
   std::string Generated;
-  std::string Encrypted;
+  std::string random_word = word_list[rand() % (sizeof(word_list) / sizeof(word_list[0]))];
   unsigned short length;
+  std::string* pgen;
 
-  PW_Management()
-  {
-    random_word = word_list[rand() % 4];
-  }
+
 
   std::string Generate_Password(unsigned short length)
   {
-    std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                        "abcdefghijklmnopqrstuvwxyz"
-                        "0123456789"
-                        "!@#$%^&*()";
+  std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                      "abcdefghijklmnopqrstuvwxyz"
+                      "0123456789"
+                      "!@#$%^&*()";
 
-    Generated.clear(); // In case it's reused
-    for (int i = 0; i < length; i++)
+    for(int i = 0; i < length; i++)
     {
       Generated += chars[rand() % chars.length()];
     }
 
     return Generated;
-  }
+  } 
 
-  std::string Encrypt_Password(std::string input)
+  std::string Encrypted;
+  std::string Encrypt_Password(std::string Generated)
   {
-    Encrypted = input;
-    for (int i = 0; i < Encrypted.length(); i++)
+    std::string Encrypted = Generated;
+    for (int i = 0; i < Generated.length(); i++)
     {
       Encrypted[i] ^= random_word[i % random_word.length()];
     }
+
     return Encrypted;
   }
 
-  std::string Cipher(std::string input)
+  std::string Cipher(std::string Encrypted)
   {
-    std::string Deciphered = input;
-    for (int i = 0; i < Deciphered.length(); i++)
+    std::string Deciphered = Encrypted;
+    for(int i = 0; i < Deciphered.length(); i++)
     {
       Deciphered[i] ^= random_word[i % random_word.length()];
     }
+
     return Deciphered;
   }
 };
 
+
 int main() {
-  srand(static_cast<unsigned>(time(nullptr)));
-
+  
+  
+  
   PW_Management Manager;
+  Manager.Generated;
+  unsigned short Menu_Choice;
 
-  std::cout << "Enter a Number: ";
+  std::cout << "What is the Length of the Password ";
 
-  while (true)
+  while(true)
   {
-    if (!(std::cin >> Manager.length) || Manager.length > 250)
+    if(!(std::cin >> Manager.length) || Manager.length < 0 || Manager.length > 250)
     {
       std::cerr << "You have Inputted an \"INVALID SHORT INTEGER\"\nTry Again: ";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       continue;
-    }
-    break;
+    } 
+  break;
   }
 
-  std::cout << "Password:  " << Manager.Generate_Password(Manager.length) << "\n";
-  std::cout << "Encrypted: " << Manager.Encrypt_Password(Manager.Generated) << "\n";
-  std::cout << "Decrypted: " << Manager.Cipher(Manager.Encrypted) << "\n";
+  switch(Menu_Choice)
+  {
+    case 1:
+    std::cout << "What is the Length of the Password ";
+
+    while(true)
+    {
+    if(!(std::cin >> Manager.length) || Manager.length < 0 || Manager.length > 250)
+    {
+      std::cerr << "You have Inputted an \"INVALID SHORT INTEGER\"\nTry Again: ";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    } 
+    break;
+  }
+  }
+
+  std::cout << "Password: " << Manager.Generate_Password(Manager.length) << "\n";
+
+  Manager.Encrypted = Manager.Encrypt_Password(Manager.Generated); 
+  std::cout << "Encrypted: " << Manager.Encrypted << "\n";
+
+  std::cout << Manager.Cipher(Manager.Encrypted);
 
   return EXIT_SUCCESS;
 }
+
