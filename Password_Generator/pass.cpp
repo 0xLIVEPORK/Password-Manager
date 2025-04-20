5b3f29133bad3461801ad4ee27813a91279774d4
EVILPORK <0xlivepork@gmail.com>
2025-04-20 22:44:10 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -4,13 +4,34 @@
 #include <string>
 #include <fstream>
 #include <ctime>
-#include logic.h
 
-int main()
+struct PW_Management
 {
-  masterkey
-  std::cout << "Enter The Master Key";
+  std::string word_list[4] = 
+  {
+    "She Said Take Your Time", 
+    "Don't Have to Rush",
+    "RAGHHHHHHHHHHHHHHH",
+    "IDK WHAT TF IM DOING"
+  };
 
-  std::cin >> 
-  
-}
+  std::string Generated;
+  std::string random_word = word_list[rand() % (sizeof(word_list) / sizeof(word_list[0]))];
+  unsigned short length;
+  std::string* pgen;
+
+
+
+  std::string Generate_Password(unsigned short length)
+  {
+  std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                      "abcdefghijklmnopqrstuvwxyz"
+                      "0123456789"
+                      "!@#$%^&*()";
+
+    for(int i = 0; i < length; i++)
+    {
+      Generated += chars[rand() % chars.length()];
+    }
+
+
