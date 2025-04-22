393b64fc1e66310fb2df944f173fb420cdcb0eed
EVILPORK <0xlivepork@gmail.com>
2025-04-22 22:24:11 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
-#include <limits>
 
-class Person{
-  std::string first;
-  std::string last;
+/*🧪 YOUR TASK
 
-  void printfullname() 
+Make a simple class called Enemy with the following:
+
+    Variables:
+
+        string type
+
+        int strength
+
+    Function:
+
+        void describe() — should output:
+        "Enemy Type: <type>, Strength: <strength>"
+
+    In main(), create 2 different enemies and call describe() on both.*/
+
+class Enemy{
+
+  public:
+
+  std::string type;
+  int strength;
+
+  void describe()
   {
-  std::cout << first << " " << last << "\n";
+    std::cout << "Enemy Type: " << type << ", Strength: " << strength << "\n";
   }
+};
 
-];
+int main() {
+
+  Enemy Vampire;
+  Vampire.type = "Undead";
+  Vampire.strength = 20;
+
+  Enemy Zombie;
+  Zombie.type = "Undead";
+  Zombie.strength = 5;
+
+  Vampire.describe();
+
+  Zombie.describe();
+
+
+}
