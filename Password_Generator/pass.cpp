c381e700717f4166caed6710689125c9d2b4b416
EVILPORK <0xlivepork@gmail.com>
2025-04-28 22:12:31 +0800

    Update pass.cpp

--- Password_Generator/pass.cpp
+++ Password_Generator/pass.cpp
@@ -17,56 +17,75 @@ class user{
 class passmanager: protected user{
     private:
 
+    std::string password;
     std::string key = "whyisthisshitopenforeveryonetoreadahaha....";
-    unsigned short choice;
+    int choice;
+    int length;
 
     public:
 
-    void generatePassword(){
-        
+    std::string generatePassword(int &length){
+        const std::string chars =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "0123456789"
+            "!@#$%^&*()";
 
-    }    
-
-    void Menu(){
-        std::cout << "\tPASSWORD MANAGER\n\n"
-              << "[1] Generate Password\n"
-              << "[2] Saved Users\n"
-              << "[3] Exit\n"
-              << "\nChoose a Number: ";
-
-        while(true)
-        {
-            if(!(std::cin >> choice) || choice > 3) {
-                std::cerr << "Invalid Input\n";
-
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-                continue;
-            }
-            break;
+        for(int i = 0; i < length; i++) {
+            password += chars[rand() % chars.size()];
         }
 
-        switch(choice)
-
-        case 1:
-
-
-        case 2:
-
-        default:
-        std::cout << "Out of Bounds";
-        
+        return password;
     }
 
-
+    void Menu() {
+        while (true) {
+            std::cout << "\tPASSWORD MANAGER\n\n"
+                      << "[1] Generate Password\n"
+                      << "[2] Saved Users\n"
+                      << "[3] Exit\n"
+                      << "\nChoose a Number: ";
+    
+            if (!(std::cin >> choice) || choice > 3 || choice < 1) {
+                std::cerr << "Invalid Input\n";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+    
+            switch (choice) {
+                case 1:
+                    while (true) {
+                        std::cout << "Enter a Length: ";
+                        if (!(std::cin >> length) || length > 200 || length < 1) {
+                            std::cerr << "Invalid Input\n";
+                            std::cin.clear();
+                            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                            continue;
+                        }
+                        break;
+                    }
+                    password.clear(); // Clear before generating
+                    std::cout << generatePassword(length) << std::endl;
+                    break;
+    
+                case 2:
+                    std::cout << "Saved Users (not implemented yet)\n";
+                    break;
+    
+                case 3:
+                    std::cout << "Exiting...\n";
+                    return; // Exit the Menu
+            }
+        }
+    }
 };
 
 int main() {
-    passmanager PasswordManager;
     srand(time(0));
-
+    passmanager PasswordManager;
     
-    std::cout << "Log-in First"; // Still not in use we'll skip to the menu
+    std::cout << "Log-in First\n\n"; // Still not in use we'll skip to the menu
 
     PasswordManager.Menu();
 
